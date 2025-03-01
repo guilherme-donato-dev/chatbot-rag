c388830a2e35baf597309060de96873f2faf165b
guilherme-donato-dev <donatocruzg@gmail.com>
2025-03-01 14:03:28 -0300

    modificado o os

--- rag_chatbot/app.py
+++ rag_chatbot/app.py
@@ -3,7 +3,6 @@ import tempfile
 
 import streamlit as st
 
-from decouple import config
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
@@ -14,7 +13,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
-os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 persist_directory = 'db'
 
 
